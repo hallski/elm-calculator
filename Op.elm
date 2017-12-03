59c22e1a5812719e83ac06c19a73ecf370678dfd
Mikael Hallendal <hallski@hallski.org>
2017-12-03 01:08:41 +0100

    Clean up Op-code

--- Op.elm
+++ Op.elm
@@ -4,56 +4,70 @@ import Html exposing (Html, button, text)
 import Html.Events exposing (onClick)
 
 
-type Op = None | Add | Minus | Multi | Div | Eql
+type Op
+    = None
+    | Add
+    | Minus
+    | Multi
+    | Div
+    | Eql
+
+
+divide : Float -> Float -> Result String Float
+divide lhs rhs =
+    case rhs of
+        0 ->
+            Err "Inf"
+
+        rhs ->
+            Ok <| lhs / rhs
+
+
+ok : (Float -> Float -> Float) -> (Float -> Float -> Result String Float)
+ok op =
+    \lhs rhs ->
+        op lhs rhs |> Ok
+
+
+okLhs : Float -> Float -> Result String Float
+okLhs v _ =
+    Ok v
+
+
+getOp : Op -> (String, Float -> Float -> Result String Float)
+getOp op =
+    case op of
+        Add ->
+            ("+", ok (+))
+
+        Minus ->
+            ("-", ok (-))
+
+        Multi ->
+            ("*", ok (*))
+
+        Div ->
+            ("/", divide)
+
+        Eql ->
+            ("=", okLhs)
+
+        None ->
+            ("", okLhs)
 
 
 executeOp : Op -> Result String Float -> Float -> Result String Float
 executeOp op result rhs =
-    Result.andThen (eo op rhs) result
+    let
+        f = (flip <| Tuple.second <| getOp op) <| rhs
+    in
+        Result.andThen f result
 
-eo op rhs lhs =
-    case (op, rhs) of
-        (Add, rhs) ->
-            Ok <| lhs + rhs
-
-        (Minus, rhs) ->
-            Ok <| lhs - rhs
-
-        (Multi, rhs) ->
-            Ok <| lhs * rhs
-
-        (Div, 0) ->
-            Err "Inf"
-
-        (Div, rhs) ->
-            Ok <| lhs / rhs
-
-        (Eql, _) ->
-            Ok lhs
-
-        (None, _) ->
-            Ok lhs
 
 toString : Op -> String
-toString op =
-    case op of
-        Add ->
-            "+"
+toString =
+    Tuple.first << getOp
 
-        Minus ->
-            "-"
-
-        Multi ->
-            "*"
-
-        Div ->
-            "/"
-
-        Eql ->
-            "="
-
-        None ->
-            ""
 
 viewOpButton : (Op -> msg) -> Op -> Html msg
 viewOpButton msg op =
